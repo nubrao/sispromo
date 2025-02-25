35e92c7ec05d2982b8bc68868d1c9fbe89cc191b
Bruno Camara <bruno_martinez@outlook.com>
2025-02-25 16:04:01 -0300

    Refactor Vite configuration by removing createHtmlPlugin and updating build settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { createHtmlPlugin } from 'vite-plugin-mpa'
 
 export default defineConfig({
-  plugins: [react(),
-  createHtmlPlugin({
-    pages: { index: "index.html" }
-  })],
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   server: {
     host: "0.0.0.0",
     port: 5173,
